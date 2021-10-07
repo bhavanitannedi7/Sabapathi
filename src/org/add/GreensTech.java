49556855f29c8241cbf8c8cc5b93bc5625ac35f9
bhavanitannedi7 <bhavani.thannidi@gmail.com>
2021-10-07 11:42:00 +0530

    sebaaaaaaaaaa

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,34 @@
+package org.add;
+
+public class GreensTech {
+
+	private void greensOmr() {
+		System.out.println("greens OMR");
+
+	}
+	private void greensAdyar() {
+		
+		System.out.println("greens Adyar");
+
+	}
+private void greensTambaram() {
+	System.out.println("greesTambaram");
+}
+private void greensVelacherry() {
+	System.out.println("greemsvelacherry");
+
+}
+private void greensAnnanagar() {
+	System.out.println("greens Annanagar");
+
+}
+public static void main(String[] args) {
+	GreensTech G = new GreensTech();
+	G.greensAdyar();
+	G.greensAnnanagar();
+	G.greensOmr();
+	G.greensTambaram();
+	G.greensVelacherry();
+}
+}
+
